da5e96a7ddea1ec4692a8bd6ea22dae6ac712b30
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-21 00:02:36 +0700

    Update git.py

--- AyiinXd/core/git.py
+++ AyiinXd/core/git.py
@@ -61,4 +61,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGS.info("Fetched Updates from Nande-Telethon")
+        LOGS.info("Fetched Updates from iwans - 𝚄𝚜𝚎𝚛𝚋𝚘𝚝")
