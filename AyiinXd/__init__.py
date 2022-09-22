3cb48895963f7cc0b40e4d49d9f6b82dbb1b200c
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-22 19:36:40 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -166,7 +166,7 @@ GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
-UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL", "https://github.com/WANSUPORT/iwans-userbot")
+UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL",https://github.com/WANSUPORT/iwans-userbot.git")
 
 # Custom Name Sticker Pack
 S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
